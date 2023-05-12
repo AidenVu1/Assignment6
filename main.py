565cfca448964c3f8ccaf22e491d3e99358a8ed8
Aiden Vu <100857445+AidenVu1@users.noreply.github.com>
2023-05-12 14:19:33 +0000

    commit

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+def calculate_pi(iterations):
+    pi = 0
+    sign = 1
 
+    for i in range(1, iterations * 2, 2):
+        pi += sign * (4/i)
+        sign *= -1
+
+    return pi
+
+iterations = 1
+finished = False
+while not finished:
+    print("Current number of iterations:", iterations)
+    print("Estimated value of Pi:", calculate_pi(iterations))
+    print()
+
+    user_input = input("Enter a number of iterations or 'q' to quit: ")
+    if user_input == 'q':
+        finished = True
+
+    if user_input == str:
+        raise ValueError
+
+    try:
+        iterations = int(user_input)
+    except ValueError:
+        print("Invalid input. Please enter a number or 'q'.")
